d135f4a8f44d124d803c999e7a10a6fa796123fd
David Wilmot <david.wilmot@rithum.com>
2026-04-26 18:37:50 +0000

    Fix the app loading problem.

--- shadow_loom_ui/__main__.py
+++ shadow_loom_ui/__main__.py
@@ -1,2 +1,11 @@
 """Allow running as: python -m shadow_loom_ui"""
-from shadow_loom_ui.app import *  # noqa: F401, F403
+from shadow_loom_ui.app import config, ui  # noqa: F401
+
+ui.run(
+    host="0.0.0.0",
+    port=7860,
+    title="Shadow Loom",
+    storage_secret=config.STORAGE_SECRET,
+    dark=True,
+    reload=False,
+)
